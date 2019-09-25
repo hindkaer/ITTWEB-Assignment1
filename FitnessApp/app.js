cc1f87a0cb9723025767d470e8e4c2f535bf25f6
Marius <mariusslmoeller@gmail.com>
2019-09-25 10:54:35 +0200

    mongodb client driver added

--- FitnessApp/app.js
+++ FitnessApp/app.js
@@ -11,19 +11,6 @@ var excerciseRouter = require('./app_server/routes/excercise');
 
 var app = express();
 
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
-
-// Connection URL
-var url = 'mongodb://localhost:27017/mydata';
-
-// Use connect method to connect to the server
-MongoClient.connect(url, function (err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-  db.close();
-});
-
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'ejs');
