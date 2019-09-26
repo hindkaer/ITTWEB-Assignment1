5d9558ff2818e1340756e640c14f09cc182cc390
Marius <mariusslmoeller@gmail.com>
2019-09-26 17:38:15 +0200

    smørkager

--- FitnessApp/app_server/models/workout.js
+++ FitnessApp/app_server/models/workout.js
@@ -5,7 +5,7 @@ const WorkoutSchema = new Schema({
     "id": { type: String, required: true, unique: true },
     "name": { type: String, required: true },
     "userid": { type: String, required: true },
-    "exercises": { type: Array, default: null }
+    "exercises": { type: [], default: null }
 });
 
-var User = module.exports = mongoose.model('User', UserSchema);
+var Workout = module.exports = mongoose.model('Workout', WorkoutSchema);
