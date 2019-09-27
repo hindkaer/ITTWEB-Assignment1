44997b7cb8121b9fb46c77e73a11f89eaae62753
Marius <mariusslmoeller@gmail.com>
2019-09-27 13:44:54 +0200

    bug fix

--- FitnessApp/app_server/config/auth.js
+++ FitnessApp/app_server/config/auth.js
@@ -1,6 +1,5 @@
 module.exports = {
     ensureAuthenticated: function (req, res, next) {
-        console.log(req.session.passport.user)
         if (req.isAuthenticated()) {
             next();
         }
