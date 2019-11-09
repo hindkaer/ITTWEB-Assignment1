b9f9a633333a6af89dc984399b62a22d5fb9dfe5
Christoffer Jakobsen <42830641+ChristofferKJ@users.noreply.github.com>
2019-11-09 17:40:30 +0100

    gargamel

--- FitnessApp/app_server/routes/api/workout.js
+++ FitnessApp/app_server/routes/api/workout.js
@@ -5,7 +5,7 @@ var verify = require('../../config/jwt')
 const controller = require('../../controllers/api/workout')
 //Base path localhost:3000/api/workout/..
 router.post('/', verify.verifyToken, controller.create)
-router.delete('/delete', verify.verifyToken, controller.delete)
+router.post('/delete', verify.verifyToken, controller.delete)
 router.get('/all', controller.getAll)
 router.get('/user', verify.verifyToken, controller.getAllForUser)
 router.post('/single', verify.verifyToken, controller.getSingle)
