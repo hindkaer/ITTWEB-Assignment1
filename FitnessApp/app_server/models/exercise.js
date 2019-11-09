6aca7accf0decc1d81aee5079cc54c2317d517e1
Marius <mariusslmoeller@gmail.com>
2019-11-09 18:53:13 +0100

    qwesdwqe

--- FitnessApp/app_server/models/exercise.js
+++ FitnessApp/app_server/models/exercise.js
@@ -4,8 +4,8 @@ var Schema = mongoose.Schema;
 const ExerciseSchema = new Schema({
     "name": { type: String, required: true },
     "description": { type: String, required: true },
-    "sets": { type: Number, default: 0 },
-    "repetitions": { type: Number, default: 0 }
+    "sets": { type: String, default: 0 },
+    "repetitions": { type: String, default: 0 }
 });
 
 var Exercise = module.exports = mongoose.model('Exercise', ExerciseSchema);
